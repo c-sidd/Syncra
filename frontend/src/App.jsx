55be60204b83e280906dba729f1843aaa6c73c45
Chandrachud Siddharth <chandrachudsiddharth@gmail.com>
2026-08-17 00:26:33 +0530

    feat: add protected account route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,29 +5,18 @@ import PrivateRoute from './components/PrivateRoute';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Account from './pages/Account';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        {/* Core application body wrapper */}
         <div className="min-h-screen bg-gray-950 flex flex-col selection:bg-purple-500 selection:text-white">
           <Navbar />
           <Routes>
-            {/* Public authentication page */}
             <Route path="/login" element={<Login />} />
-            
-            {/* Private guarded dashboard explorer route */}
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <Dashboard />
-                </PrivateRoute>
-              }
-            />
-            
-            {/* Catch-all redirect to Dashboard */}
+            <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
