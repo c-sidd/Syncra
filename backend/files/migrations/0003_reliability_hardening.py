14b7ac9f11b0f7fbc01ea85bce689d139d2de824
Chandrachud Siddharth <chandrachudsiddharth@gmail.com>
2026-09-09 00:11:16 +0530

    feat: add reliability migrations and upload cleanup

---
+++ backend/files/migrations/0003_reliability_hardening.py
@@ -0,0 +1,17 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [('files', '0002_file_object_key_file_storage_class_and_more')]
+
+    operations = [
+        migrations.AddField(
+            model_name='uploadsession',
+            name='part_count',
+            field=models.PositiveIntegerField(default=0),
+        ),
+        migrations.AddIndex(model_name='file', index=models.Index(fields=['user', 'deleted_at'], name='files_file_user_id_deleted_idx')),
+        migrations.AddIndex(model_name='file', index=models.Index(fields=['user', 'folder'], name='files_file_user_id_folder_idx')),
+        migrations.AddIndex(model_name='uploadsession', index=models.Index(fields=['state', 'expires_at'], name='files_upload_state_exp_idx')),
+        migrations.AddIndex(model_name='uploadsession', index=models.Index(fields=['user', 'state'], name='files_upload_user_state_idx')),
+    ]
